4c84282732331a340f584f7e4689005d3c436339
Jordan Minx <char6447@yahoo.com>
2020-03-12 16:33:57 -0500

    Create numpy_arrays.py

---
+++ numpy_arrays.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import numpy as np
+
+sess = tf.Session()
+
+zeroD = np.array(30, dtype=np.int32)
+sess.run(tf.rank(zeroD))
+
+oneD = np.array([5.6, 6.3, 8.9, 9.0], dtype=np.float32)
+sess.run(tf.rank(oneD))
